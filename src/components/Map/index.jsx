cc8cc7489ab43d8fc51b69e7d789e3907891a29e
Denison Kolling <denisonkolling@gmail.com>
2024-02-07 18:00:10 -0300

    feat: add map component

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,29 @@
+import { TileLayer } from 'react-leaflet';
+import { MapContainerStyled, MarkerStyled, PopupStyled } from './styles';
+
+const Map = ({ positions, center }) => {
+	return (
+		<MapContainerStyled center={center} zoom={13}>
+			<TileLayer
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				attribution='&copy; <a
+					href="https://www.openstreetmap.org/copyright">&nbsp;OpenStreetMap&nbsp;</a> contributors'
+			/>
+
+			{positions.map((vehicle, index) => {
+				return (
+					<MarkerStyled position={[vehicle.latitude, vehicle.longitude]} key={index}>
+						<PopupStyled>
+							<p>
+								<strong>Plate</strong> {vehicle.license_plate}<br />
+                <strong>Ignition {vehicle.ignition ? "On" : "Off"}</strong>
+								<strong>Speed</strong> {vehicle.speed}<br />									
+							</p>
+						</PopupStyled>
+					</MarkerStyled>
+				)
+			})}
+		</MapContainerStyled>
+	);
+};
+export default Map;
---
+++ src/components/Map/styles.js
@@ -0,0 +1,8 @@
+import { MapContainer, Marker, Popup } from 'react-leaflet';
+import styled from "styled-components";
+export const MapContainerStyled = styled(MapContainer)`
+ height: 90%;
+ border: 1px solid darkgrey;
+`
+export const MarkerStyled = styled(Marker)``
+export const PopupStyled = styled(Popup)``
